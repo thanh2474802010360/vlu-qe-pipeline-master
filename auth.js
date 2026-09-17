76d5d08a371e292ac210aa8a98bd5392e110d281
Ngô Vinh Thành <thanh.2474802010360@vanlanguni.vn>
2026-09-17 17:08:28 +0700

    Fix authentication logic

--- auth.js
+++ auth.js
@@ -9,7 +9,7 @@ function login(username, password) {
         return false;
     }
 
-    return username === 'admin' && password === '9999';
+    return username === 'admin' && password === '123';
 }
 
 module.exports = login;
